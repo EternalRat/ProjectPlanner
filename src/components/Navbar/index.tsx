b0a3b4285b9de9c0103c3ea0d1c18dbf5387e287
HENRY Benjamin <benjamin1.henry@epitech.eu>
2022-11-02 17:16:31 +0100

    added a sample for navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,23 @@
 export function Navbar() {
-    return <div className="navbar"></div>;
+    require("./index.css");
+    return <div className="navbar">
+        <div className="header">
+            <div className="user">ze</div>
+        </div>
+        <hr></hr>
+        <div className="sections">
+            <div className="section">
+                <span>Dashboard</span>
+            </div>
+            <div className="section">
+                <span>Groups</span>
+            </div>
+            <div className="section">
+                <span>Todo</span>
+            </div>
+        </div>
+        <div className="footer">
+            <div className="contact">?</div>
+        </div>
+    </div>;
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../../components/Navbar";
 
 export function Dashboard() {
+    require("./index.css");
     return <div className="app">
         <Navbar></Navbar>
         <div className="content"></div>
